70e3ee8163d4ea526667d743c038429822dc0e3d
Chitroopa Manikkavasagam, Marcel Nogueira and Starlynne Start <starlynne@>
2017-03-29 14:01:06 -0700

    Add logic for spec4 and spec4 is valid

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,17 @@ var consonant = ["b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v"
 var pigLatin = function (wordParameter) {
   wordParameter.forEach(function(word){
   if (word.match(/\D/))
-  { if (word.match(/^[aeiou]/)){
+  { if (word.match(/^[aeiou]/))
+    {
       word = word + "ay";
     }
+    else if (word.match(/^qu/))
+    {
+      var len = word.length;
+      var pigWord = word.slice(0,2);
+      var pigword1 = word.slice(2,len);
+      word = pigword1 + pigWord + "ay"
+      }
     // word = word + "ay";
     // alert("number");
   }
