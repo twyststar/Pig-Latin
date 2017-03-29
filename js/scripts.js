745b2589c79ed40696c47751c8d037828fda29f8
Chitroopa Manikkavasagam, Marcel Nogueira and Starlynne Start <starlynne@>
2017-03-29 15:57:15 -0700

    fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,63 @@
+
 var pigLatin = function (wordParameter) {
+  var latin = [];
   wordParameter.forEach(function(word){
   if (word.match(/\D/))
-  { if (word.match(/^[aeiou]/i))
+  {
+    if (word.match(/^[aeiou]/i))
     {
       word = word + "ay";
+      latin.push(word);
+      return latin;
     }
     else if (word.match(/^squ/i))
     {
       var len = word.length;
       var pigWord = word.slice(0,3);
       var pigword1 = word.slice(3,len);
-      word = pigword1 + pigWord + "ay"
+      word = pigword1 + pigWord + "ay";
+      latin.push(word);
+      return latin;
     }
     else if ((word.match(/^qu/i)) || (word.match(/^[b-df-h-j-np-tv-z][b-df-h-j-np-tv-z]/i)))
     {
       var len = word.length;
       var pigWord = word.slice(0,2);
       var pigword1 = word.slice(2,len);
-      word = pigword1 + pigWord + "ay"
+      word = pigword1 + pigWord + "ay";
+      latin.push(word);
+      return latin;
       }
       else  if (word.match(/^[b-df-h-j-np-tv-z]/i))
       {
         var len = word.length;
         var pigWord = word.slice(0,1);
         var pigword1 = word.slice(1,len);
-        word = pigword1 + pigWord + "ay"
+        word = pigword1 + pigWord + "ay";
+        latin.push(word);
+        return latin;
       }
 
-
-    // word = word + "ay";
-    // alert("number");
+      latin.push(word);
+      return latin;
   }
 
-   console.log(word);
+  latin.push(word);
+  return latin;
 
   });
+ var result = latin.join(" ");
+ return result;
 };
-
-
-
-
 
 $(document).ready(function(){
   $("#form1").submit(function(event){
     event.preventDefault();
     var words = [];
     words = ($("#input").val()).split(" ");
-    console.log(words);
     var result = pigLatin(words);
+    $("#result").text(result);
+    $("#result-div").show();
   });
 
 });
