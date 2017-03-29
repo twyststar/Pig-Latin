3c9d1e280397f78352eaf401cb34a77a1607903f
Chitroopa Manikkavasagam, Marcel Nogueira and Starlynne Start <starlynne@>
2017-03-29 10:46:20 -0700

    Add logic for spec1 and spec1 is valid

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 
+var vowels = ["a","e","i","o","u"];
+var consonant = ["b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","w","x","y","z"];
+
 var pigLatin = function (wordParameter) {
+  wordParameter.forEach(function(word){
+   word = word + "ay";
+   console.log(word);
 
+  });
+};
 
-}
 
 
 
@@ -13,6 +20,7 @@ $(document).ready(function(){
     var words = [];
     words = ($("#input").val()).split(" ");
     console.log(words);
+    var result = pigLatin(words);
   });
 
 });
