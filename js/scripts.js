62bf858c8647ae399b501f47621799cd457f2ccd
Chitroopa Manikkavasagam, Marcel Nogueira and Starlynne Start <chitroopa@>
2017-03-29 14:26:20 -0700

    Add logic for integrate specs 5,6 and spec 7

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-
-var vowels = ["a","e","i","o","u"];
-var consonant = ["b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","w","x","y","z"];
-
 var pigLatin = function (wordParameter) {
   wordParameter.forEach(function(word){
   if (word.match(/\D/))
@@ -9,13 +5,29 @@ var pigLatin = function (wordParameter) {
     {
       word = word + "ay";
     }
-    else if (word.match(/^qu/))
+    else if (word.match(/^squ/))
+    {
+      var len = word.length;
+      var pigWord = word.slice(0,3);
+      var pigword1 = word.slice(3,len);
+      word = pigword1 + pigWord + "ay"
+    }
+    else if ((word.match(/^qu/)) || (word.match(/^[bcdfghjklmnpqrstvwxyz][bcdfghjklmnpqrstvwxyz]/)))
     {
       var len = word.length;
       var pigWord = word.slice(0,2);
       var pigword1 = word.slice(2,len);
       word = pigword1 + pigWord + "ay"
       }
+      else  if (word.match(/^[bcdfghjklmnpqrstvwxyz]/))
+      {
+        var len = word.length;
+        var pigWord = word.slice(0,1);
+        var pigword1 = word.slice(1,len);
+        word = pigword1 + pigWord + "ay"
+      }
+
+
     // word = word + "ay";
     // alert("number");
   }
