37f9df7bba37145a4f38db474787352b1e860551
Chitroopa Manikkavasagam, Marcel Nogueira and Starlynne Start <marcel@>
2017-03-29 14:46:10 -0700

    add global logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 var pigLatin = function (wordParameter) {
   wordParameter.forEach(function(word){
   if (word.match(/\D/))
-  { if (word.match(/^[aeiou]/))
+  { if (word.match(/^[aeiou]/i))
     {
       word = word + "ay";
     }
-    else if (word.match(/^squ/))
+    else if (word.match(/^squ/i))
     {
       var len = word.length;
       var pigWord = word.slice(0,3);
       var pigword1 = word.slice(3,len);
       word = pigword1 + pigWord + "ay"
     }
-    else if ((word.match(/^qu/)) || (word.match(/^[bcdfghjklmnpqrstvwxyz][bcdfghjklmnpqrstvwxyz]/)))
+    else if ((word.match(/^qu/i)) || (word.match(/^[b-df-h-j-np-tv-z][b-df-h-j-np-tv-z]/i)))
     {
       var len = word.length;
       var pigWord = word.slice(0,2);
       var pigword1 = word.slice(2,len);
       word = pigword1 + pigWord + "ay"
       }
-      else  if (word.match(/^[bcdfghjklmnpqrstvwxyz]/))
+      else  if (word.match(/^[b-df-h-j-np-tv-z]/i))
       {
         var len = word.length;
         var pigWord = word.slice(0,1);
