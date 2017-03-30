e2657ddd12a671b96900da3009db0ccf66022acb
Starlynne <lewis.start0905@gmail.com>
2017-03-29 19:05:27 -0700

    Move variable to DRY up code.

--- js/scripts.js
+++ js/scripts.js
@@ -2,48 +2,44 @@
 var pigLatin = function (wordParameter) {
   var latin = [];
   wordParameter.forEach(function(word){
+      var len = word.length;
   if (word.match(/\D/))
   {
     if (word.match(/^[aeiou]/i))
     {
       word = word + "ay";
       latin.push(word);
-      return latin;
+      return word
     }
     else if (word.match(/^squ/i))
     {
-      var len = word.length;
+
       var pigWord = word.slice(0,3);
       var pigword1 = word.slice(3,len);
       word = pigword1 + pigWord + "ay";
       latin.push(word);
-      return latin;
+      return word
     }
     else if ((word.match(/^qu/i)) || (word.match(/^[b-df-h-j-np-tv-z][b-df-h-j-np-tv-z]/i)))
     {
-      var len = word.length;
-      var pigWord = word.slice(0,2);
-      var pigword1 = word.slice(2,len);
+
+    var pigWord = word.slice(0,2);
+    var pigword1 = word.slice(2,len);
+    word = pigword1 + pigWord + "ay";
+    latin.push(word);
+    return word
+    }
+    else  if (word.match(/^[b-df-h-j-np-tv-z]/i))
+    {
+
+      var pigWord = word.slice(0,1);
+      var pigword1 = word.slice(1,len);
       word = pigword1 + pigWord + "ay";
       latin.push(word);
-      return latin;
-      }
-      else  if (word.match(/^[b-df-h-j-np-tv-z]/i))
-      {
-        var len = word.length;
-        var pigWord = word.slice(0,1);
-        var pigword1 = word.slice(1,len);
-        word = pigword1 + pigWord + "ay";
-        latin.push(word);
-        return latin;
-      }
-
-      latin.push(word);
-      return latin;
+      return word
+    }
   }
-
   latin.push(word);
-  return latin;
 
   });
  var result = latin.join(" ");
